bf6e629339630582030de1e7e35a1db9c37abd07
shajiawang <khliu.tw@gmail.com>
2020-07-08 23:40:27 +0800

    改用Anko 1.AnkoLogger 2.聯網讀取資料字串及以uiThread區塊控制UI

--- app/src/main/java/tw/khliu/shop/ParkingActivity.kt
+++ app/src/main/java/tw/khliu/shop/ParkingActivity.kt
@@ -4,15 +4,26 @@ import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_parking.*
+import org.jetbrains.anko.*
 import java.net.URL
 
-class ParkingActivity : AppCompatActivity() {
+class ParkingActivity : AppCompatActivity() , AnkoLogger{
     private val _TAG=this::class.java.simpleName
     private val _URL = "http://www.saja.com.tw/site/mall/?json=Y"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_parking)
-        ParkingTask().execute(_URL)
+        doAsync {
+            val url = URL(_URL)
+            val json = url.readText()
+            info(json)
+            uiThread{
+                toast("Got it !!")
+                // alert("Got it !!","ALERT"){
+                tv_parking.setText(json)
+            }
+        }
+        // ParkingTask().execute(_URL)
     }
 
     inner class ParkingTask:AsyncTask<String, Void, String>() {
